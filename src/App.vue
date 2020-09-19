977ce00e1b8e72b6c31cbedcbc0d78f9a0bf3f53
Eric Lam <ehylam.p@gmail.com>
2020-09-19 20:20:50 +1000

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Hello, world" />
+  <Posts title="A SINGLE post.." content="Lorem ipsum dolor something something." />
+  <Posts
+    title="SUPA FAST"
+    content="Lorem ipsum dolor something something. Lorem ipsum dolor something something. Lorem ipsum dolor something something. Lorem ipsum dolor something something."
+  />
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+import Posts from "./components/Posts.vue";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+    Posts,
+  },
+};
+</script>
---
+++ src/components/Posts.vue
@@ -0,0 +1,20 @@
+<template>
+  <section>
+    <h1 class="title">{{title}}</h1>
+    <p class="content">{{content}}</p>
+  </section>
+</template>
+
+
+<script>
+export default {
+  name: "Posts",
+  props: {
+    title: String,
+    content: String,
+  },
+};
+</script>
+
+
+<style lang="stylus"></style>
