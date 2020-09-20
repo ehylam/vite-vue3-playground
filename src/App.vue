71b13c6a52d6015b30e4a163e15d18e8f33f600b
Eric Lam <ehylam.p@gmail.com>
2020-09-20 14:06:09 +1000

    async fetch api data

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,5 @@
+
+
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Hello, world" />
@@ -9,14 +11,39 @@
 </template>
 
 <script>
+import { onMounted, reactive } from "vue";
 import HelloWorld from "./components/HelloWorld.vue";
 import Posts from "./components/Posts.vue";
+
+// http://localhost:8888/tailwind/wp-json/wp/v2
 
 export default {
   name: "App",
   components: {
     HelloWorld,
     Posts,
+  },
+  data: () => ({
+    posts: {
+      title: "test",
+      content: "testing",
+    },
+  }),
+  setup() {
+    const posts = reactive({ posts: [] });
+
+    onMounted(() => {
+      fetch("http://localhost:8888/tailwind/wp-json/wp/v2/posts")
+        .then((res) => {
+          if (res.status !== 200) {
+          } else {
+            return res.json();
+          }
+        })
+        .then((res) => {
+          posts.posts.push(res);
+        });
+    });
   },
 };
 </script>
